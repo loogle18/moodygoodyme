6fd8001ee85312ad16fadd2861bb4810446afeeb
moodygoody <sviat.minato@gmail.com>
2016-10-23 23:58:21 +0300

    Improved header and home page. Generated contacts controller.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def main_content_classes
     [controller_name, '-', action_name].join
   end
+
+  def nav_black_or_white_class
+    controller_name == 'home' ? '-black' : '-white'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   root to: 'home#show'
+
+  resources :contacts, only: :index
 end
