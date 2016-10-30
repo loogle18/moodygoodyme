3677e031b90fe7cb046d288f0e9e3ed279f4a6f7
moodygoody <sviat.minato@gmail.com>
2016-10-30 11:21:36 +0200

    Fixed links for touch devices. Added puma config.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,4 @@
 class ContactsController < ApplicationController
   def index
-    respond_to do |format|
-      format.html
-      format.json {
-        render json: { html: render_to_string('contacts/index') }
-      }
-    end
   end
 end
