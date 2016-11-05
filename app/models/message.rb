70ef64264b2428a2f783898194d8695594b9b2aa
moodygoody <sviat.minato@gmail.com>
2016-11-05 23:52:38 +0200

    Update home page with about section. Changed validation text for email abd body of new message.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 class Message < ApplicationRecord
-  validates :email, email_format: { message: "can't be blank or invalid" }
-  validates :body, presence: true
+  validates :email, email_format: { message: 'should contain valid email' }
+  validates :body, presence: { message: 'should contain your message' }
 end
